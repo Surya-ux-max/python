ed5ac9b2482926c07e88af7a029ed814a7630198
Surya-ux-max <surprakas14@gmail.com>
2026-05-27 10:11:56 +0530

    Add initial function definitions and examples in funtion.py

---
+++ prj1/funtion.py
@@ -0,0 +1,39 @@
+# def greet():
+#     print("Hello, welcome to the world of programming!")
+
+# greet()
+# greet()
+# greet()       ## def is the keyword , greet is the function name, () is used to call the function and : is used to indicate the start of the function body.
+
+
+# def greet(name):
+#     print("Hello " + name + ", welcome to the world of programming!")
+# greet("Surya");
+# greet("Mugesh");
+
+
+## here now we adding a parameter to the function, which is name. When we call the function, we pass an argument (Surya and Mugesh) to the function, which is then used in the function body to print a personalized greeting message.
+
+
+# def add(a,b):
+#     return a+b;
+
+# print(add(10,20))   ## this will give an error because the function add is not defined yet.
+
+# def greet(name):
+#     return ""
+# print(greet("Surya"));  ## this will give an error because the function greet is not defined yet.
+
+
+# Return statement
+
+# def add(a,b):
+#     return a+b;
+
+# a = int(input("Enter a number: "));
+# b = int(input("Enter another number: "));
+
+# result = add(a,b);
+# print(result);   ## this will give an error because the function add is not defined yet.
+
+
